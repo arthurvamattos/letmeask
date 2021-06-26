e37bd6901fb30181114a5850872db79aa7c040dc
Arthur Vinicius <arthurviniciusvha@gmail.com>
2021-06-26 14:21:45 -0400

    update localstorage key name

--- src/hooks/usePersistedState.ts
+++ src/hooks/usePersistedState.ts
@@ -4,7 +4,9 @@ type Response<T> = [T, Dispatch<SetStateAction<T>>];
 
 function usePersistedState<T>(key: string, initialState: T): Response<T> {
   const [state, setState] = useState(() => {
-    const storageValue = localStorage.getItem(key);
+    const storageValue = localStorage.getItem(
+      `@letmeask-arthurvamattos:${key}`
+    );
 
     if (storageValue) {
       return JSON.parse(storageValue);
